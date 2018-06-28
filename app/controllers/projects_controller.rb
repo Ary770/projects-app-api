19676064a1eb9bb1027b959ff1a86d52016f0d8c
github username <github email address>
2018-06-28 15:53:33 -0400

    updated client

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,11 +6,17 @@ class ProjectsController < ApplicationController
 
   def show
     project = Project.find_by(id: params[:id])
-    binding.pry
     render json: project, status: 201
   end
 
   def create
     binding.pry
+  
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :category_id, :notes, :start_by, :finish_by)
   end
 end
